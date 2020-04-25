69c8e4b04112510e95b98f69ca4dbe54ef5a31dd
matilda-art <heysober@163.com>
2020-04-25 17:23:30 +0800

    练习

---
+++ 删除链表中等于给定值 val 的所有节点/src/TestDemo.java
@@ -0,0 +1,34 @@
+/**
+ * @program: 删除链表中等于给定值 val 的所有节点
+ * @description
+ * @author: matilda
+ * @create: 2020-04-25 16:40
+ **/
+class Node{
+    public int data;
+    public Node next;
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class TestDemo {
+    public Node head;
+
+    public void removeElements(int val) {
+        Node prev = this.head;
+        Node cur = this.head.next;
+        while (cur != null){
+           if (cur.data == val){
+               prev.next = cur.next;
+               cur = cur.next;
+            }
+            prev = cur;
+            cur = cur.next;
+        }
+        if (this.head.data == val){
+            this.head = this.head.next;
+        }
+    }
+}
