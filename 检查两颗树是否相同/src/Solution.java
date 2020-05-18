f4203cc08ba13e2d5e80bb8bd3e6b23159440da0
matilda-art <heysober@163.com>
2020-05-18 10:49:07 +0800

    检查两棵树是否相同

---
+++ 检查两颗树是否相同/src/Solution.java
@@ -0,0 +1,30 @@
+
+/**
+ * @program: 检查两颗树是否相同
+ * @description
+ * @author: matilda
+ * @create: 2020-05-18 10:38
+ **/
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) {
+          val = x;
+      }
+  }
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (q == null && p == null){
+            return true;
+        }
+        if ((p != null && q == null) || (p == null && q != null)) {
+            return false;
+        }
+        if (p.val != q.val){
+            return false;
+        }
+        return isSameTree(p.left, q.left)&&isSameTree(p.right,q.right);
+    }
+}
